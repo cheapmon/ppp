3bfe68567ed3d0d5b11940b14f1f2bb76f91d1a9
Simon Kaleschke <simon@SEIMS-PC.local>
2016-12-02 12:02:49 +0100

    add: new extraction structure (4 scripts) add: newest db with updated xpaths del: old db, single scripts

---
+++ script/PrivacyDiffer.rb
@@ -0,0 +1,23 @@
+require 'differ'
+
+module PrivacyDiffer
+
+	class << self
+		attr_accessor :threshold
+	end
+
+	self.threshold = 5
+
+	def self.different? s1, s2
+		diff = Differ.diff_by_word(s1, s2).format_as(:ascii)
+		changes = 0
+		diff.scan(/\{[\+\-]\".*\"\}/).each do |m|
+			changes += m.split.size
+		end
+		changes += diff.scan(/\{\"\w*\" >> \"\w*\"\}/).size
+		words = s1.split.size
+		percent_c = (changes.to_f)/(words.to_f)*100
+		(percent_c >= self.threshold)
+	end
+
+end
