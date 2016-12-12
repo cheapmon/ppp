533e84aec058dbaf809b275be40b08db24ee2b8e
Contused <alex.pr@gmx.de>
2016-12-12 22:02:45 +0100

    add: dropdown buttons with dates.

--- tmc_frontend/app/src/index_directive.js
+++ tmc_frontend/app/src/index_directive.js
@@ -1,6 +1,6 @@
-angular.module('demo',[]).controller('Hello', function($scope, $http){
- $http.get('http://localhost:8080/rest/datum?company=google').then(function(response){
-        $scope.greeting = response.data;
-     console.log($scope.greeting);
+angular.module('tmc',[]).controller('pageCtrl', function($scope, $http){
+    $scope.siteName = "google";
+    $http.get('http://localhost:8080/rest/datum?company=' + $scope.siteName).then(function(response){
+        $scope.dates = response.data;
     });
 });
