b0b15b3b98f709b35faf5986081e40d094593968
Contused <alex.pr@gmx.de>
2016-12-14 17:55:22 +0100

    add: Prototype Diff tool now functioning!

--- tmcrawl_backend/src/main/java/resources/TextService.java
+++ tmcrawl_backend/src/main/java/resources/TextService.java
@@ -10,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import tools.TextLoader;
@@ -19,11 +20,11 @@ import tools.TextLoader;
 public class TextService{
 	
 	@GET
-	@Path("{date}")
+	@Path("{company}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public static List<Texts> getTexts(@PathParam("date") String date) throws SQLException {
+	public static List<Texts> getTexts(@PathParam("company") String company, @QueryParam("date") String date) throws SQLException {
 		TextLoader tl = new TextLoader();
-		List<Texts> allTexts = tl.getTexts(date);
+		List<Texts> allTexts = tl.getTexts(company, date);
 		
 		
 		return allTexts;
