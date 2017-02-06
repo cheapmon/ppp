858dd8fd02ce7cf7600f9d7cec1d5408d65e2659
Jörn <mountpixel@googlemail.com>
2017-02-06 12:58:01 +0100

    upd: add link to Texts json

--- tmc_backend/src/main/java/tools/TextLoader.java
+++ tmc_backend/src/main/java/tools/TextLoader.java
@@ -12,41 +12,42 @@ import representation.Texts;
  *
  */
 public class TextLoader {
-	/**
-	 * get text of a company
-	 * @param company: Table of the company in the database
-	 * @param date: searched date of the saved policy
-	 * @return list of texts
-	 * @throws SQLException
-	 */
-	public List<Texts> getTexts(String company, String date) throws SQLException{
-		//get database instance
-		DatabaseInitializer db = DatabaseInitializer.getInstance();
-		//set and initialize db connection
-		db.connection = null;
-		db.initDBConnection();
-		//initialize result set, statement and list
-		PreparedStatement ps = null;
-    	ResultSet rs;
-    	List<Texts> allDates = new ArrayList<Texts>();
-   		try {
-			//switch: SQL for company
-			//20 standard companies
-			//default for new companies
-			String querie = "SELECT CONTENT, SYSTEM_DATE FROM "+ company.toUpperCase() + " WHERE SYSTEM_DATE = ?";
-			ps = db.connection.prepareStatement(querie);
-			ps.setString(1, date); 
-			rs = ps.executeQuery(); 
-			while (rs.next()){
-				Texts t = new Texts(rs.getString(1),rs.getString(2));
-				allDates.add(t);
-				//System.out.println(t);
-			}
-		} catch (SQLException e) {
-			System.out.println("Fehler: Datenbankabfrage");
-			e.printStackTrace();
-		} 
-		db.connection.close();
-		return allDates;
-	}
+    /**
+     * get text of a company
+     * @param company: Table of the company in the database
+     * @param date: searched date of the saved policy
+     * @param link: web link to the policy
+     * @return list of texts
+     * @throws SQLException
+     */
+    public List<Texts> getTexts(String company, String date) throws SQLException{
+        //get database instance
+        DatabaseInitializer db = DatabaseInitializer.getInstance();
+        //set and initialize db connection
+        db.connection = null;
+        db.initDBConnection();
+        //initialize result set, statement and list
+        PreparedStatement ps = null;
+        ResultSet rs;
+        List<Texts> allDates = new ArrayList<Texts>();
+        try {
+            //switch: SQL for company
+            //20 standard companies
+            //default for new companies
+            String querie = "SELECT CONTENT, SYSTEM_DATE, LINK FROM "+ company.toUpperCase() + " WHERE SYSTEM_DATE = ?";
+            ps = db.connection.prepareStatement(querie);
+            ps.setString(1, date);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                Texts t = new Texts(rs.getString(1),rs.getString(2), rs.getString(3));
+                allDates.add(t);
+                //System.out.println(t);
+            }
+        } catch (SQLException e) {
+            System.out.println("Fehler: Datenbankabfrage");
+            e.printStackTrace();
+        } 
+        db.connection.close();
+        return allDates;
+    }
 }
